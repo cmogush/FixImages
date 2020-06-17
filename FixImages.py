315f34295f8c87ddd56bb7fc9a9c871f1b62e2e6
cmogush <cmogush@gmail.com>
2020-06-17 14:05:21 -0400

    Adjusted logic for FixImages

--- FixImages.py
+++ FixImages.py
@@ -3,30 +3,26 @@ from PIL import Image
 import os
 from pathlib import Path
 
-src = r"C:\Users\Chris\Desktop\Python Scripts\FixImages\Images"
-dest = r"C:\Users\Chris\Desktop\Python Scripts\FixImages\Icons"
+src = r"/home/student-03-fac5da9edff2/images"
+dest = r"/opt/icons"
 
 def fixImage(file):
     # open image
     with Image.open(file) as img:
-        # convert alpha channel
-        if img.mode in ('RGBA', 'LA'):
-            background = Image.new(img.mode[:-1], img.size, 'white')
-            background.paste(img, img.split()[-1])
-            img = background
 
         # rotate 90 deg
         out = img.rotate(90) # counter clockwise
-
         # resize to 128
         out = out.resize((128, 128))
 
         # save as jpg in new dest
         f, e = os.path.splitext(os.path.basename(file))
-        outfile = dest + "\\" + f + ".jpg"
-        out.save(outfile, "JPEG")
+    outfile = dest + "/" + f
+    out.save(outfile, "JPEG")
 
 for file in os.listdir(src):
-    f, e = os.path.splitext(file)
-    if e == ".tiff":
-        fixImage(src+"\\"+file)
+     try:
+         print("fixing {}".format(file))
+         fixImage(src+"/"+file)
+     except:
+         print("{} not image".format(file))
